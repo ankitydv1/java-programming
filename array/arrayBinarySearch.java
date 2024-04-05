199a4ce11c5635813caa5bce294407fe1248472f
Ankit Yadav <146722364+ankitydv1@users.noreply.github.com>
2024-04-05 11:50:07 +0530

    container config done

---
+++ array/arrayBinarySearch.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class arrayBinarySearch {
+
+    public static void main(String args[]){
+        int numbers[] = {2,4,5,8,9,10,12,13};
+        int key = 10;
+        f()
+    }
+    
+}
