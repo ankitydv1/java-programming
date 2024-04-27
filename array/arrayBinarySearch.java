b1809d6b3e3577a45b4c82fc1f30c37355c98181
Ankit Yadav <146722364+ankitydv1@users.noreply.github.com>
2024-04-27 14:59:53 +0530

    container config done

--- array/arrayBinarySearch.java
+++ array/arrayBinarySearch.java
@@ -1,10 +1,27 @@
 import java.util.*;
 public class arrayBinarySearch {
+    public static int binarySearch(int numbers[], int key){
+         int start = 0, end = numbers.length-1;
+
+         while(start <= end){
+            int mid = (start+end)/2;
+
+            //comparisons
+            if(numbers[mid] == key) {
+                return mid;
+            }
+            if(numbers[mid] < key){ //right
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+         }
+         return -1;
+    }
 
     public static void main(String args[]){
         int numbers[] = {2,4,5,8,9,10,12,13};
         int key = 10;
-        f()
+        System.out.println("index for key is : " + binarySearch(numbers, key));
     }
-    
 }
